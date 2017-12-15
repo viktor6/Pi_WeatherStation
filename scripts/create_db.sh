46b26ed9e29d2c387ef45625d26f06093ded2f43
viktor6 <viktor6@ukr.net>
2017-12-15 23:02:42 +0200

    update

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -5,12 +5,14 @@ then { echo "Создаю базу данных";
 
 # Procesor
 rrdtool create weatherRRD.rrd --start N --step 300 \
-DS:tempo:GAUGE:600:U:U \
 DS:tempi:GAUGE:600:U:U \
-DS:humid:GAUGE:600:U:U \
 DS:press:GAUGE:600:U:U \
 DS:altit:GAUGE:600:U:U \
-DS:luxmeter:GAUGE:600:U:U \
+DS:tempta:GAUGE:600:U:U \
+DS:temptb:GAUGE:600:U:U \
+DS:temptc:GAUGE:600:U:U \
+DS:temptd:GAUGE:600:U:U \
+DS:temptg:GAUGE:600:U:U \
 RRA:AVERAGE:0.5:1:600 \
 RRA:AVERAGE:0.5:6:700 \
 RRA:AVERAGE:0.5:24:775 \
--- scripts/readsensors.py
+++ scripts/readsensors.py
@@ -78,7 +78,78 @@ def readDS18B20b():
 	else:
 		dbgprint("Error reading DS18B20 sensor")
 	return tempRead	
+
+#------------------------------------------------------------------------
+# readDS18B20 - Read DS18B20 sensor (exterior temperature)
+#------------------------------------------------------------------------
+def readDS18B20c():
 	
+	tempRead = -100.0
+	cmdStat, cmdOut = commands.getstatusoutput("cat /sys/bus/w1/devices/28-7d4358126461/w1_slave")
+	dbgprint("DS18B20 output: " + str(cmdOut))
+
+	# Parse output
+	if cmdStat == 0:
+		matchObj = re.search(r'.*crc=\S*\s(\S*)[\n].*t=(.*)',cmdOut,re.I)
+		if matchObj:
+			if matchObj.group(1) == 'YES':
+				tempRead = float(int(matchObj.group(2))/1000.0)
+				dbgprint("Exterior Temperature: "+str(tempRead))
+			else:
+				dbgprint("DS18B20 returned CRC error.")
+		else:
+			dbgprint("No match during DS18B20 output parse!!")
+	else:
+		dbgprint("Error reading DS18B20 sensor")
+	return tempRead
+
+#------------------------------------------------------------------------
+# readDS18B20 - Read DS18B20 sensor (exterior temperature)
+#------------------------------------------------------------------------
+def readDS18B20d():
+	
+	tempRead = -100.0
+	cmdStat, cmdOut = commands.getstatusoutput("cat /sys/bus/w1/devices/28-7d4358126461/w1_slave")
+	dbgprint("DS18B20 output: " + str(cmdOut))
+
+	# Parse output
+	if cmdStat == 0:
+		matchObj = re.search(r'.*crc=\S*\s(\S*)[\n].*t=(.*)',cmdOut,re.I)
+		if matchObj:
+			if matchObj.group(1) == 'YES':
+				tempRead = float(int(matchObj.group(2))/1000.0)
+				dbgprint("Exterior Temperature: "+str(tempRead))
+			else:
+				dbgprint("DS18B20 returned CRC error.")
+		else:
+			dbgprint("No match during DS18B20 output parse!!")
+	else:
+		dbgprint("Error reading DS18B20 sensor")
+	return tempRead	
+
+#------------------------------------------------------------------------
+# readDS18B20 - Read DS18B20 sensor (exterior temperature)
+#------------------------------------------------------------------------
+def readDS18B20g():
+	
+	tempRead = -100.0
+	cmdStat, cmdOut = commands.getstatusoutput("cat /sys/bus/w1/devices/28-7d4358126461/w1_slave")
+	dbgprint("DS18B20 output: " + str(cmdOut))
+
+	# Parse output
+	if cmdStat == 0:
+		matchObj = re.search(r'.*crc=\S*\s(\S*)[\n].*t=(.*)',cmdOut,re.I)
+		if matchObj:
+			if matchObj.group(1) == 'YES':
+				tempRead = float(int(matchObj.group(2))/1000.0)
+				dbgprint("Exterior Temperature: "+str(tempRead))
+			else:
+				dbgprint("DS18B20 returned CRC error.")
+		else:
+			dbgprint("No match during DS18B20 output parse!!")
+	else:
+		dbgprint("Error reading DS18B20 sensor")
+	return tempRead		
 #------------------------------------------------------------------------
 # readBMP085 - Read BMP085 sensor or BMP180 (Interior temperature, atmospheric pressure and altitude sensor)
 #------------------------------------------------------------------------
@@ -95,7 +166,7 @@ def readBMP085():
 #------------------------------------------------------------------------
 # writeHTML - Write HTML block to show current values
 #------------------------------------------------------------------------
-def writeHTML(to, ti, hu, pa, al, lu, tb):
+def writeHTML(ti, pa, al, ta, tb, tc, td, tg):
     vNow = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
     html = open("/var/www/html/web/current.html", "w")	
 	
@@ -105,14 +176,6 @@ def writeHTML(to, ti, hu, pa, al, lu, tb):
     html.write('  	  				<div class="panel-heading" style="text-align:left">Дата и время последнего обновления1: '+str(vNow)+'</div>\n')
     html.write('					<div class="panel-body">\n')
     html.write('		<div class="row">\n')
-    html.write('			<div class="col-md-2">\n')
-    html.write('  				<div class="panel panel-info">\n')
-    html.write('  	  				<div class="panel-heading">Наружная температура</div>\n')
-    html.write('					<div class="panel-body" style="text-align: center">\n')
-    html.write('						<h1>'+str(round(to,1))+'<small> &deg;C</small></h1>\n')
-    html.write('					</div>\n')
-    html.write('				</div>\n')
-    html.write('			</div>\n')
     html.write('			<div class="col-md-2">\n')
     html.write('  				<div class="panel panel-warning">\n')
     html.write('  	  				<div class="panel-heading">Температура в комнате</div>\n')
@@ -133,7 +196,7 @@ def writeHTML(to, ti, hu, pa, al, lu, tb):
     html.write('  				<div class="panel panel-danger">\n')
     html.write('  	  				<div class="panel-heading">Наружная температура1</div>\n')
     html.write('					<div class="panel-body" style="text-align: center">\n')
-    html.write('						<h1>'+str(hu)+'<small> &deg;C</small></h1>\n')
+    html.write('						<h1>'+str(round(al))+'<small> m</small></h1>\n')
     html.write('					</div>\n')
     html.write('				</div>\n')
     html.write('			</div>\n')
@@ -141,7 +204,7 @@ def writeHTML(to, ti, hu, pa, al, lu, tb):
     html.write('  				<div class="panel panel-warning">\n')
     html.write('  	  				<div class="panel-heading">Наружная температура2</div>\n')
     html.write('					<div class="panel-body" style="text-align: center">\n')
-    html.write('						<h1>'+str(round(al))+'<small> &deg;C</small></h1>\n')
+    html.write('						<h1>'+str(round(ta,1))+'<small> &deg;C</small></h1>\n')
     html.write('					</div>\n')
     html.write('				</div>\n')
     html.write('			</div>\n')
@@ -154,33 +217,50 @@ def writeHTML(to, ti, hu, pa, al, lu, tb):
     html.write('				</div>\n')
     html.write('			</div>\n')
     html.write('			<div class="col-md-2">\n')
+    html.write('  				<div class="panel panel-warning">\n')
+    html.write('  	  				<div class="panel-heading">Наружная температура2</div>\n')
+    html.write('					<div class="panel-body" style="text-align: center">\n')
+    html.write('						<h1>'+str(round(tc,1))+'<small> &deg;C</small></h1>\n')
+    html.write('					</div>\n')
+    html.write('				</div>\n')
+    html.write('			</div>\n')
+    html.write('			<div class="col-md-2">\n')
+    html.write('  				<div class="panel panel-warning">\n')
+    html.write('  	  				<div class="panel-heading">Наружная температура tb</div>\n')
+    html.write('					<div class="panel-body" style="text-align: center">\n')
+    html.write('						<h1>'+str(round(td,1))+'<small> &deg;C</small></h1>\n')
+    html.write('					</div>\n')
+    html.write('				</div>\n')
+    html.write('			</div>\n')	
+    html.write('			<div class="col-md-2">\n')
     html.write('  				<div class="panel panel-info">\n')
     html.write('  	  				<div class="panel-heading">Наружная температура3</div>\n')
     html.write('					<div class="panel-body" style="text-align: center">\n')
-    html.write('						<h1>'+str(round(lu,1))+'<small> &deg;C</small></h1>\n')
+    html.write('						<h1>'+str(round(tg,1))+'<small> &deg;C</small></h1>\n')
     html.write('					</div>\n')
     html.write('				</div>\n')
     html.close()
 
     curr = open("/var/www/html/web/current.txt", "w")
-    curr.write(str(vNow)+';'+str(round(to,1))+';'+str(ti)+';'+str(round(pa))+';'+str(hu)+';'+str(al)+';'+str(lu)+';'+str(tb)+'\n')
+    curr.write(str(vNow)+';'+str(ti)+';'+str(round(pa))+';'+str(al)+';'+str(round(ta,1))+';'+str(round(tb,1))+';'+str(round(tc,1))+';'+str(round(td,1))+';'+str(round(tg,1))+'\n')
     curr.close()
 
 #------------------------------------------------------------------------
 # Main program
 #------------------------------------------------------------------------
 
-tempExt = readDS18B20a()
-humid = readDS18B20a()
-tempi, press, altit = readBMP085()
-luxmeter = readDS18B20a()
+tempta = readDS18B20a()
 temptb = readDS18B20b()
+temptc = readDS18B20c()
+temptd = readDS18B20d()
+temptg = readDS18B20g()
+tempi, press, altit = readBMP085()
 #write to RRD archive
-cmd = '/usr/bin/rrdtool update /home/pi/bazy/weatherRRD.rrd -t tempo:tempi:press:humid:altit:luxmeter:temptb N:'
-cmd = cmd +str(tempExt)+':'+str(tempi)+':'+str(press)+':'+str(humid)+':'+str(altit)+':'+str(luxmeter)+':'+str(temptb)
+cmd = '/usr/bin/rrdtool update /home/pi/bazy/weatherRRD.rrd -t tempi:press:altit:tempExt:temptb:temptc:temptd:temptg N:'
+cmd = cmd +str(tempi)+':'+str(press)+':'+str(altit)+':'+str(tempta)+':'+str(temptb)+':'+str(temptc)+':'+str(temptd)+':'+str(temptg)
 cmdStat, cmdOut = commands.getstatusoutput(cmd)
 dbgprint("RRD write result: "+str(cmdStat))
 
-writeHTML(tempExt, tempi, humid, press, altit, luxmeter, temptb)
+writeHTML(tempi, press, altit, tempta, temptb, temptc, temptd, temptg)
 
 dbgprint ("End!")
